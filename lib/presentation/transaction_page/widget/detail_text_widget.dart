efc3b3317040765f45c817fc70b8e4518b464c5e
Freezsx <bblacks011@gmail.com>
2024-07-29 13:16:17 +0700

    style : [MA] add order detail

--- lib/presentation/transaction_page/widget/detail_text_widget.dart
+++ lib/presentation/transaction_page/widget/detail_text_widget.dart
@@ -6,30 +6,44 @@ class DetailDataWidget extends StatelessWidget {
   final String leftTitle;
   final String rightTitle;
 
+  final leftTitleStyle, rightTitleStyle;
+  final leftTitleAlignment, rightTitleAlignment;
+  final textTitleOverflow;
+
   const DetailDataWidget({
     required this.leftTitle,
     required this.rightTitle,
+    this.leftTitleStyle,
+    this.rightTitleStyle,
+    this.leftTitleAlignment,
+    this.rightTitleAlignment,
+    this.textTitleOverflow,
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height: 10),
         Container(
           margin: EdgeInsets.symmetric(horizontal: 5),
           child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
                 leftTitle,
-                style: tsBodySmallRegular(darkBlue),
-                textAlign: TextAlign.left,
+                style: leftTitleStyle ?? tsBodySmallRegular(darkGrey),
+                textAlign: leftTitleAlignment ?? TextAlign.left,
               ),
-              Text(
-                rightTitle,
-                style: tsBodySmallRegular(darkBlue),
-                textAlign: TextAlign.right,
+              Container(
+                width: 200,
+                child: Text(
+                  rightTitle,
+                  style: rightTitleStyle ?? tsBodySmallRegular(black),
+                  textAlign: rightTitleAlignment ?? TextAlign.right,
+                  overflow: textTitleOverflow ?? TextOverflow.ellipsis,
+                  maxLines: 3,
+                ),
               ),
             ],
           ),
---
+++ lib/widget/common/mainpage_appbar_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../infrastructure/theme/themes.dart';
+
+class MainpageAppbarWidget extends StatelessWidget
+    implements PreferredSizeWidget {
+  const MainpageAppbarWidget({
+    Key? key,
+    this.onPressed,
+    this.title,
+    this.icon,
+    this.backgroundColor,
+    this.textStyle,
+    this.elevation,
+    this.noLeading = false,
+  }) : super(key: key);
+
+  final VoidCallback? onPressed;
+  final String? title;
+  final IconData? icon;
+  final Color? backgroundColor;
+  final TextStyle? textStyle;
+  final bool noLeading;
+  final double? elevation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: noLeading
+          ? null
+          : IconButton(
+        icon: Icon(
+          icon ?? Icons.arrow_back_ios_rounded,
+          color: darkGrey,
+          size: 20,
+        ),
+        onPressed: onPressed ?? Get.back,
+      ),
+      elevation: elevation,
+      automaticallyImplyLeading: !noLeading,
+      backgroundColor: backgroundColor,
+      title: Text(
+        title ?? 'default text title',
+        style: textStyle ?? tsBodyMediumMedium(black),
+      ),
+      centerTitle: true,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
