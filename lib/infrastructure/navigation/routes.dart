2e144cfe4b72df51e9d99a7f8ebff7712668b2fd
Adipp <emangadayanglain@gmail.com>
2024-08-06 00:51:13 +0700

    feat: [A] firebase notification integration and create notification feature

--- lib/infrastructure/navigation/routes.dart
+++ lib/infrastructure/navigation/routes.dart
@@ -1,7 +1,7 @@
 class Routes {
   static Future<String> get initialRoute async {
     // TODO: implement method
-    return NAVBAR;
+    return LOGIN_PAGE;
   }
 
   static const HOME_PAGE = '/home-page';
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:washit_admin/infrastructure/dal/services/firebase_api.dart';
+import 'package:washit_admin/infrastructure/dal/services/firebase_options.dart';
 
 import 'infrastructure/navigation/navigation.dart';
 import 'infrastructure/navigation/routes.dart';
 import 'infrastructure/theme/themes.dart';
 
 void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseApi.initNotification();
   var initialRoute = await Routes.initialRoute;
   runApp(Main(initialRoute));
 }
